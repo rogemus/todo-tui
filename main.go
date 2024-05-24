f764a1b797b22174ccda70a61f754d1ef93aa850
Kacper Rogowski <rogemus@gmail.com>
2024-05-24 15:47:32 +0200

    master: Add inprogress list. Add ability to add/remove to list.

--- main.go
+++ main.go
@@ -1,54 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
-	"sort"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
 	cursor      int
-	currentList string
 	lists       map[string]List
+	currentList int
+	listsKeys   []string
 }
 
 func initialModel() model {
 	return model{
 		lists: map[string]List{
+			"inprogress": {
+				Title:       "In Progress",
+				Placeholder: "[No item on the list. Select any item and press `s` to start work on the item.]",
+				Items: []Item{
+					{Title: "Test", Desc: "Lorem", Done: false},
+				},
+			},
 			"todo": {
-				Title: "Todo",
+				Title:       "Todo",
+				Placeholder: "[No item on the list. Press `a` to add new item.]",
 				Items: []Item{
 					{Title: "Test", Desc: "Lorem", Done: false},
 				},
 			},
 			"done": {
-				Title: "Done",
+				Title:       "Done",
+				Placeholder: "[No item on the list. Select any item and press `c` to complete it.]",
 				Items: []Item{
 					{Title: "Test", Desc: "Lorem", Done: true},
 				},
 			},
 		},
-		currentList: "todo",
+		currentList: 0,
+		listsKeys:   []string{"inprogress", "todo", "done"},
 	}
 }
 
 type Item struct {
-	Title  string
-	Desc   string
-	Done   bool
-	ListId string
+	Title string
+	Desc  string
+	Done  bool
 }
 
 type List struct {
-	Title string
-	Items []Item
+	Title       string
+	Placeholder string
+	Items       []Item
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
+
+
+var ErrEmptyList = errors.New("Empty list")
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -61,36 +75,88 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor--
 			}
 		case "down", "j":
-			if m.cursor < len(m.lists[m.currentList].Items)-1 {
+			listKey := m.listsKeys[m.currentList]
+
+			if m.cursor < len(m.lists[listKey].Items)-1 {
 				m.cursor++
 			}
 		case "tab":
-			if m.currentList == "todo" {
-				m.currentList = "done"
+			if m.currentList == len(m.listsKeys)-1 {
+				m.currentList = 0
 			} else {
-				m.currentList = "todo"
+				m.currentList += 1
 			}
+		case "s":
+			listKey := m.listsKeys[m.currentList]
+			list := m.lists[listKey]
+			item := list.Items[m.cursor]
+			nextListKey := m.listsKeys[0]
 
-			m.cursor = 0
-		case "enter":
-			item := m.lists[m.currentList].Items[m.cursor]
-			nextList := ""
-
-			if m.currentList == "todo" {
-				nextList = "done"
-			} else {
-				nextList = "todo"
-			}
-
-			if list, ok := m.lists[m.currentList]; ok {
+			// Move item to Done list
+			if list, ok := m.lists[listKey]; ok {
 				list.Items = append(list.Items[:m.cursor], list.Items[m.cursor+1:]...)
-				m.lists[m.currentList] = list
+				m.lists[listKey] = list
 			}
 
-			if list, ok := m.lists[nextList]; ok {
+			if list, ok := m.lists[nextListKey]; ok {
+				list.Items = append(list.Items, item)
+				m.lists[nextListKey] = list
+			}
+		case "a":
+			item := Item{Title: "New item", Done: false, Desc: ""}
+
+			if list, ok := m.lists[m.listsKeys[1]]; ok {
+				list.Items = append(list.Items, item)
+				m.lists[m.listsKeys[1]] = list
+			}
+		case "c":
+			listKey := m.listsKeys[m.currentList]
+			list := m.lists[listKey]
+			item := list.Items[m.cursor]
+			nextListKey := m.listsKeys[2]
+
+			// Move item to Done list
+			if list, ok := m.lists[listKey]; ok {
+				list.Items = append(list.Items[:m.cursor], list.Items[m.cursor+1:]...)
+				m.lists[listKey] = list
+			}
+
+			if list, ok := m.lists[nextListKey]; ok {
 				item.Done = !item.Done
 				list.Items = append(list.Items, item)
-				m.lists[nextList] = list
+				m.lists[nextListKey] = list
+			}
+
+		case "d":
+			listKey := m.listsKeys[m.currentList]
+
+			if list, ok := m.lists[listKey]; ok {
+				list.Items = append(list.Items[:m.cursor], list.Items[m.cursor+1:]...)
+				m.lists[listKey] = list
+			}
+		case "enter":
+			listKey := m.listsKeys[m.currentList]
+			nextListKey := m.listsKeys[2]
+
+			if m.cursor < len(m.lists[listKey].Items) {
+				item := m.lists[listKey].Items[m.cursor]
+
+				// Move item to Todo list
+				if item.Done {
+					nextListKey = m.listsKeys[1]
+				}
+
+				// Move item to Done list
+				if list, ok := m.lists[listKey]; ok {
+					list.Items = append(list.Items[:m.cursor], list.Items[m.cursor+1:]...)
+					m.lists[listKey] = list
+				}
+
+				if list, ok := m.lists[nextListKey]; ok {
+					item.Done = !item.Done
+					list.Items = append(list.Items, item)
+					m.lists[nextListKey] = list
+				}
 			}
 		}
 	}
@@ -101,15 +167,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	s := "What should we buy at the market?\n\n"
 
-	keys := make([]string, 0)
-	for k := range m.lists {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	for _, key := range keys {
+	for listIndex, key := range m.listsKeys {
 		list := m.lists[key]
 		s += fmt.Sprintf("%s:\n", list.Title)
+
+		if len(list.Items) == 0 {
+			s += list.Placeholder
+		}
 
 		for i, item := range list.Items {
 			cursor := " "
@@ -119,7 +183,7 @@ func (m model) View() string {
 				checked = "x"
 			}
 
-			if key == m.currentList {
+			if listIndex == m.currentList {
 				if m.cursor == i {
 					cursor = ">"
 				}
